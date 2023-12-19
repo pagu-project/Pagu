3fd74f1616a2c1ca470a4058c4226419f39659ac
Kay <kehiiiiya@gmail.com>
2023-12-19 16:39:42 +0000

    feat: enhancing tx hash in faucet message

--- discord/discord.go
+++ discord/discord.go
@@ -267,7 +267,7 @@ func (b *Bot) messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 				log.Printf("error saving faucet information: %v\n", err)
 			}
 
-			msg := p.Sprintf("%v  %.4f tPAC's is staked to %v successfully🪙!\n with %v as referral.\n your transaction hash:%v",
+			msg := p.Sprintf("%v  %.4f tPAC's is staked to %v successfully🪙!\n with %v as referral.\nYour transaction hash: ```%v```",
 				m.Author.Username, amount, address, referral.DiscordName, txHashFaucet)
 			_, _ = s.ChannelMessageSendReply(m.ChannelID, msg, m.Reference())
 			return
@@ -310,7 +310,7 @@ func (b *Bot) messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 			if err != nil {
 				log.Printf("error saving faucet information: %v\n", err)
 			}
-			msg := p.Sprintf("%v  %.4f tPAC's is staked to %v successfully🪙!\nyour transaction hash:%v",
+			msg := p.Sprintf("%v  %.4f tPAC's is staked to %v successfully🪙!\nYour transaction hash: ```%v```",
 				m.Author.Username, b.cfg.FaucetAmount, trimmedAddress, txHash)
 			_, _ = s.ChannelMessageSendReply(m.ChannelID, msg, m.Reference())
 
