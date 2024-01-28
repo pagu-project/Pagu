5ed54f7a83e8c8a509d0ae9d67335137d98e56d6
izik <bwanfomisaac@gmail.com>
2024-01-29 00:57:26 +0100

    feat: adding commands embeds to bot

--- discord/embeds.go
+++ discord/embeds.go
@@ -14,3 +14,38 @@ func helpEmbed(s *discordgo.Session) *discordgo.MessageEmbed {
 		Description: "RoboPac is a robot that provides support and information about the Pactus Blockchain.",
 	}
 }
+
+func claimEmbed(s *discordgo.Session, i *discordgo.InteractionCreate, result string) *discordgo.MessageEmbed {
+	return &discordgo.MessageEmbed{
+		Title:       "claim Pactus Coins",
+		Description: result,
+	}
+}
+
+func claimerInfoEmbed(s *discordgo.Session, i *discordgo.InteractionCreate, result string) *discordgo.MessageEmbed {
+	return &discordgo.MessageEmbed{
+		Title:       "claimer info",
+		Description: result,
+	}
+}
+
+func nodeInfoEmbed(s *discordgo.Session, i *discordgo.InteractionCreate, result string) *discordgo.MessageEmbed {
+	return &discordgo.MessageEmbed{
+		Title:       "node info",
+		Description: result,
+	}
+}
+
+func networkHealthEmbed(s *discordgo.Session, i *discordgo.InteractionCreate, result string) *discordgo.MessageEmbed {
+	return &discordgo.MessageEmbed{
+		Title:       "network health",
+		Description: result,
+	}
+}
+
+func networkStatusEmbed(s *discordgo.Session, i *discordgo.InteractionCreate, result string) *discordgo.MessageEmbed {
+	return &discordgo.MessageEmbed{
+		Title:       "network status",
+		Description: result,
+	}
+}
