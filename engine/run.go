c2aa655de9dcdc9eb4dcf21dc3eb5558f8657019
Kay <kehiiiiya@gmail.com>
2024-01-28 17:40:18 +0000

    feat: adding all commands to Run method

--- engine/run.go
+++ engine/run.go
@@ -6,7 +6,11 @@ import (
 )
 
 const (
-	botCmdClaim = "claim"
+	CmdClaim         = "claim"        //!
+	CmdClaimerInfo   = "claimer-info" //!
+	CmdNodeInfo      = "node-info"    //!
+	CmdNetworkStatus = "network"      //!
+	CmdNetworkHealth = "health"       //!
 )
 
 // The input is always string.
@@ -18,16 +22,80 @@ func (be *BotEngine) Run(input string) (string, error) {
 	cmd, args := be.parseQuery(input)
 
 	switch cmd {
-	case botCmdClaim:
+	case CmdClaim:
 		if len(args) != 3 {
 			return "", fmt.Errorf("expected to have 3 arguments, but it received %d", len(args))
 		}
 
-		_, err := be.Claim(args[0], args[1], args[2])
+		txHash, err := be.Claim(args[0], args[1], args[2])
 		if err != nil {
 			return "", err
 		}
-		return "", nil
+		return fmt.Sprintf("Reward claimed successfully✅\nYour claim transaction: https://pacscan.org/transactions/%s", txHash), nil
+
+	case CmdClaimerInfo:
+		if len(args) != 1 {
+			return "", fmt.Errorf("expected to have 1 arguments, but it received %d", len(args))
+		}
+
+		claimer, err := be.ClaimerInfo(args[0])
+		if err != nil {
+			return "", err
+		}
+
+		return fmt.Sprintf("Claimer Infoℹ️\nTestNet Address: %s\namount: %v\nIsClaimed: %v\n txHash: %s",
+			args[0], claimer.TotalReward, claimer.IsClaimed(), claimer.ClaimedTxID), nil
+
+	case CmdNetworkHealth:
+		health, err := be.NetworkHealth()
+		if err != nil {
+			return "", err
+		}
+
+		var status string
+		if health.HealthStatus {
+			status = "Healthy✅"
+		} else {
+			status = "UnHealthy❌"
+		}
+
+		return fmt.Sprintf("NetWork health status🧑‍⚕️\nNetwork Is: %s\nCurrentTime: %v\nLastBlockTime: %v\nTime Diff: %v\nLast Block Height: %v",
+			status, health.CurrentTime.Format("02/01/2006, 15:04:05"), health.LastBlockTime.Format("02/01/2006, 15:04:05"), health.TimeDifference, health.LastBlockHeight), nil
+
+	case CmdNodeInfo:
+		if len(args) != 1 {
+			return "", fmt.Errorf("expected to have 1 arguments, but it received %d", len(args))
+		}
+
+		nodeInfo, err := be.NodeInfo(args[0])
+		if err != nil {
+			return "", err
+		}
+
+		var pip19Score string
+		if nodeInfo.AvailabilityScore >= 0.9 {
+			pip19Score = fmt.Sprintf("%v✅", nodeInfo.AvailabilityScore)
+		} else {
+			pip19Score = fmt.Sprintf("%v⚠️", nodeInfo.AvailabilityScore)
+		}
+
+		return fmt.Sprintf("NodeInfo🛟\nPeerID: %s\nIP Address: %s\nAgent: %s\n"+
+			"Moniker: %s\nCountry: %s\nCity: %s\nRegion Name: %s\nTimeZone: %s\n"+
+			"ISP: %s\n\nValidator Info🔍\nNumber: %v\nPIP19-Score: %s\nStake: %v\n",
+			nodeInfo.PeerID, nodeInfo.IPAddress, nodeInfo.Agent, nodeInfo.Moniker, nodeInfo.Country,
+			nodeInfo.City, nodeInfo.RegionName, nodeInfo.TimeZone, nodeInfo.ISP, nodeInfo.ValidatorNum,
+			pip19Score, nodeInfo.StakeAmount), nil
+
+	case CmdNetworkStatus:
+		net, err := be.NetworkStatus()
+		if err != nil {
+			return "", err
+		}
+
+		return fmt.Sprintf("Network Status🕸️\nNetWork Name: %s\nConnected Peers: %v\n"+
+			"Validators Count: %v\nCurrent Block Height: %v\nTotal Power: %v\nTotal Committee Power: %v\n"+
+			"> Note📝: This info is from one random network node. so, non-blockchain data maybe won't be same every time.",
+			net.NetworkName, net.ConnectedPeersCount, net.ValidatorsCount, net.CurrentBlockHeight, net.TotalNetworkPower, net.TotalCommitteePower), nil
 
 	default:
 		return "", fmt.Errorf("unknown command: %s", cmd)
