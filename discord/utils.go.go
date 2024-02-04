9f3a9a0c12e596c3e155a10144034575432be0cc
Kay <kehiiiiya@gmail.com>
2024-02-04 10:46:03 +0000

    feat(discord): using green color for healthy status and red for unhealthy

--- discord/utils.go.go
+++ discord/utils.go.go
@@ -2,6 +2,12 @@ package discord
 
 import "github.com/bwmarrin/discordgo"
 
+const (
+	GREEN  = 0x008000
+	RED    = 0xFF0000
+	PACTUS = 0x052D5A
+)
+
 func checkMessage(i *discordgo.InteractionCreate, s *discordgo.Session, guildID, userID string) bool {
 	if i.GuildID != guildID || s.State.User.ID == userID {
 		return false
