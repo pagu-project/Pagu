a8af08eaf6719dccde3ab94fe0c276e8c6b879ee
b00f <mostafa.sedaghat@gmail.com>
2024-02-13 00:25:59 +0800

    Update discord/embeds.go
    Co-authored-by: Kay <kehiiiiya@gmail.com>

--- discord/embeds.go
+++ discord/embeds.go
@@ -22,7 +22,7 @@ func helpEmbed(s *discordgo.Session) *discordgo.MessageEmbed {
 			"```/node-info``` Shows a node and validator info in network and blockchain.\n" +
 			"```/network-status``` Shows a brief info about network.\n" +
 			"```/network-health``` Check and shows network health status.\n" +
-			"```/bot-wallet``` Shows RoboPac wallet address and balance.\n" +
+			"```/wallet``` Shows RoboPac wallet address and balance.\n" +
 			"```/twitter-campaign``` Get discount code on Twitter Campaign.\n" +
 			"```/twitter-campaign-status``` Check the status of Twitter Campaign.\n",
 		Color: PACTUS,
