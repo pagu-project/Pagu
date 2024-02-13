cc59d94d1ab2bd4e680bc7083fa58a9b7c822ba6
Kay <kehiiiiya@gmail.com>
2024-02-13 16:19:14 +0000

    Update utils.go

--- engine/utils.go
+++ engine/utils.go
@@ -3,8 +3,8 @@ package engine
 import "fmt"
 
 func CheckArgs(requiredArgs int, args []string) error {
-	if len(args) != requiredArg {
-		return fmt.Errorf("incorrect number of arguments, expected %d but got %d", requiredArg, len(args))
+	if len(args) != requiredArgs {
+		return fmt.Errorf("incorrect number of arguments, expected %d but got %d", requiredArgs, len(args))
 	}
 	return nil
 }
