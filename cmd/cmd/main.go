550e1bf10c3e33acdea2267bad54d41b77e73f31
izik <bwanfomisaac@gmail.com>
2024-02-15 21:58:10 +0100

    Removed repl subcommand

--- cmd/cmd/main.go
+++ cmd/cmd/main.go
@@ -1,18 +1,70 @@
 package main
 
 import (
+	"bufio"
 	"os"
+	"strings"
 
-	"github.com/spf13/cobra"
+	cobra "github.com/spf13/cobra"
+
+	"github.com/kehiy/RoboPac/config"
+	"github.com/kehiy/RoboPac/engine"
+	"github.com/kehiy/RoboPac/log"
+	"github.com/pactus-project/pactus/crypto"
 )
+
+const PROMPT = "\n>> "
 
 func main() {
 	rootCmd := &cobra.Command{
 		Use:     "robopac-cmd",
 		Version: "0.0.1",
-	}
+		Run: func(cmd *cobra.Command, args []string) {
 
-	REPLCommand(rootCmd)
+			log.InitGlobalLogger()
+
+			log.Info("initializing repl...")
+
+			envOpt := cmd.Flags().StringP("env", "e", ".env", "the env file path")
+			config, err := config.Load(*envOpt)
+			if err != nil {
+				log.Panic("can't load config env", "err", err, "path", *envOpt)
+			}
+
+			if config.Network == "Localnet" {
+				crypto.AddressHRP = "tpc"
+			}
+
+			botEngine, err := engine.NewBotEngine(config)
+			if err != nil {
+				log.Panic("could not start discord bot", "err", err)
+			}
+			botEngine.Start()
+
+			log.Info("repl started")
+			reader := bufio.NewReader(os.Stdin)
+
+			for {
+				cmd.Print(PROMPT)
+
+				input, _ := reader.ReadString('\n')
+				input = strings.TrimSuffix(input, "\n")
+
+				if strings.ToLower(input) == "exit" {
+					cmd.Println("exiting from repl")
+
+					return
+				}
+
+				response, err := botEngine.Run(input)
+				if err != nil {
+					cmd.PrintErr(err)
+				}
+
+				cmd.Print(response)
+			}
+		},
+	}
 
 	err := rootCmd.Execute()
 	if err != nil {
