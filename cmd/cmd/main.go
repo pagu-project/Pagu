51218ebbcd76202a662c32bc768430746117eecc
izik <bwanfomisaac@gmail.com>
2024-02-15 22:58:15 +0100

    make run function in cmd

--- cmd/cmd/main.go
+++ cmd/cmd/main.go
@@ -14,55 +14,57 @@ import (
 
 const PROMPT = "\n>> "
 
+func run(cmd *cobra.Command, args []string) {
+	log.InitGlobalLogger()
+
+	log.Info("initializing repl...")
+
+	envOpt := cmd.Flags().StringP("env", "e", ".env", "the env file path")
+	config, err := config.Load(*envOpt)
+	if err != nil {
+		log.Panic("can't load config env", "err", err, "path", *envOpt)
+	}
+
+	if config.Network == "Localnet" {
+		crypto.AddressHRP = "tpc"
+	}
+
+	botEngine, err := engine.NewBotEngine(config)
+	if err != nil {
+		log.Panic("could not start discord bot", "err", err)
+	}
+
+	botEngine.Start()
+
+	log.Info("repl started")
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		cmd.Print(PROMPT)
+
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSuffix(input, "\n")
+
+		if strings.ToLower(input) == "exit" {
+			cmd.Println("exiting from repl")
+
+			return
+		}
+
+		response, err := botEngine.Run(input)
+		if err != nil {
+			cmd.PrintErr(err)
+		}
+
+		cmd.Print(response)
+	}
+}
+
 func main() {
 	rootCmd := &cobra.Command{
 		Use:     "robopac-cmd",
 		Version: "0.0.1",
-		Run: func(cmd *cobra.Command, args []string) {
-			log.InitGlobalLogger()
-
-			log.Info("initializing repl...")
-
-			envOpt := cmd.Flags().StringP("env", "e", ".env", "the env file path")
-			config, err := config.Load(*envOpt)
-			if err != nil {
-				log.Panic("can't load config env", "err", err, "path", *envOpt)
-			}
-
-			if config.Network == "Localnet" {
-				crypto.AddressHRP = "tpc"
-			}
-
-			botEngine, err := engine.NewBotEngine(config)
-			if err != nil {
-				log.Panic("could not start discord bot", "err", err)
-			}
-
-			botEngine.Start()
-
-			log.Info("repl started")
-			reader := bufio.NewReader(os.Stdin)
-
-			for {
-				cmd.Print(PROMPT)
-
-				input, _ := reader.ReadString('\n')
-				input = strings.TrimSuffix(input, "\n")
-
-				if strings.ToLower(input) == "exit" {
-					cmd.Println("exiting from repl")
-
-					return
-				}
-
-				response, err := botEngine.Run(input)
-				if err != nil {
-					cmd.PrintErr(err)
-				}
-
-				cmd.Print(response)
-			}
-		},
+		Run:     run,
 	}
 
 	err := rootCmd.Execute()
