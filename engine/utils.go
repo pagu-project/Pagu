626ed393abdcb28230b42f6d440c4c4067e75951
Mostafa <mostafa.sedaghat@gmail.com>
2024-03-04 17:50:53 +0800

    chore: fix linting issues

--- engine/utils.go
+++ engine/utils.go
@@ -1,12 +1,14 @@
 package engine
 
 func boosterPrice(allPackages int) int {
-	if allPackages < 100 {
+	switch {
+	case allPackages < 100:
 		return 30
-	} else if allPackages < 200 {
+	case allPackages < 200:
 		return 40
-	} else if allPackages < 300 {
+	case allPackages < 300:
 		return 50
+	default:
+		return 100
 	}
-	return 100
 }
