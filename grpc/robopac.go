86df40c619a98812d1712dfdb01d442ed64645d0
Kay <kehiiiiya@gmail.com>
2024-04-01 08:31:48 +0000

    fix(gRPC): rename execute to run

--- grpc/robopac.go
+++ grpc/robopac.go
@@ -18,7 +18,7 @@ func newRoboPacServer(server *Server) *robopacServer {
 	}
 }
 
-func (rs *robopacServer) Execute(_ context.Context, er *robopac.ExecuteRequest) (*robopac.ExecuteResponse, error) {
+func (rs *robopacServer) Run(_ context.Context, er *robopac.RunRequest) (*robopac.RunResponse, error) {
 	beInput := []string{}
 
 	tokens := strings.Split(er.Command, " ")
@@ -26,7 +26,7 @@ func (rs *robopacServer) Execute(_ context.Context, er *robopac.ExecuteRequest) 
 
 	res := rs.engine.Run(command.AppIdgRPC, er.Id, beInput)
 
-	return &robopac.ExecuteResponse{
+	return &robopac.RunResponse{
 		Response: res.Message,
 	}, nil
 }
