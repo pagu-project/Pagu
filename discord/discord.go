e13a95c68d689c6a6fd7af50e7a22db04eb361c9
izik <bwanfomisaac@gmail.com>
2024-04-25 20:29:57 +0100

    modified discord.go

--- discord/discord.go
+++ discord/discord.go
@@ -113,10 +113,9 @@ func (bot *DiscordBot) commandHandler(db *DiscordBot, s *discordgo.Session, i *d
 	log.Info("AppIdDiscord", command.AppIdDiscord.String())
 	// log.Info("UserID", i.User.ID)
 	log.Info("Input", beInput)
+	log.Info("Executing command with beInput: %v", discordCmd.Name)
 
 	res := db.engine.Run(command.AppIdDiscord, i.User.ID, beInput)
-
-	log.Info("Executing command with beInput: %v", beInput)
 
 	bot.respondResultMsg(res, s, i)
 }
