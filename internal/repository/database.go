99797ed050b27251e71f668e9427969dccfe61c5
mj <akbarian.dev@gmail.ccom>
2024-06-10 18:08:26 +0330

    refactor(repository): add entity and seperate repository package from entity

--- internal/repository/database.go
+++ internal/repository/database.go
@@ -1,11 +1,7 @@
 package repository
 
 import (
-	"time"
-
-	"github.com/pagu-project/Pagu/internal/repository/faucet"
-	"github.com/pagu-project/Pagu/internal/repository/user"
-	"github.com/pagu-project/Pagu/internal/repository/zealy"
+	"github.com/pagu-project/Pagu/internal/entity"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -22,13 +18,13 @@ func NewDB(path string) (*DB, error) {
 		}
 	}
 
-	if !db.Migrator().HasTable(&user.User{}) ||
-		!db.Migrator().HasTable(&faucet.Faucet{}) ||
-		!db.Migrator().HasTable(&zealy.ZealyUser{}) {
+	if !db.Migrator().HasTable(&entity.User{}) ||
+		!db.Migrator().HasTable(&entity.Faucet{}) ||
+		!db.Migrator().HasTable(&entity.ZealyUser{}) {
 		if err := db.AutoMigrate(
-			&user.User{},
-			&faucet.Faucet{},
-			&zealy.ZealyUser{},
+			&entity.User{},
+			&entity.Faucet{},
+			&entity.ZealyUser{},
 		); err != nil {
 			return nil, MigrationError{
 				Message: err.Error(),
@@ -39,120 +35,4 @@ func NewDB(path string) (*DB, error) {
 	return &DB{
 		DB: db,
 	}, nil
-}
-
-func (db *DB) AddUser(u *user.User) error {
-	tx := db.Create(u)
-	if tx.Error != nil {
-		return WriteError{
-			Message: tx.Error.Error(),
-		}
-	}
-
-	return nil
-}
-
-func (db *DB) GetUser(id string) (*user.User, error) {
-	var u *user.User
-	tx := db.Model(&user.User{}).Preload("Faucets").First(&u, "id = ?", id)
-	if tx.Error != nil {
-		return &user.User{}, ReadError{
-			Message: tx.Error.Error(),
-		}
-	}
-
-	return u, nil
-}
-
-func (db *DB) AddFaucet(f *faucet.Faucet) error {
-	tx := db.Create(f)
-	if tx.Error != nil {
-		return WriteError{
-			Message: tx.Error.Error(),
-		}
-	}
-
-	return nil
-}
-
-func (db *DB) HasUser(id string) bool {
-	var exists bool
-
-	_ = db.Model(&user.User{}).
-		Select("count(*) > 0").
-		Where("id = ?", id).
-		Find(&exists).
-		Error
-
-	return exists
-}
-
-func (db *DB) CanGetFaucet(id string) bool {
-	var u user.User
-	tx := db.Model(&user.User{}).Preload("Faucets").First(&u, "id = ?", id)
-	if tx.Error != nil {
-		return false
-	}
-
-	now := time.Now()
-
-	for _, f := range u.Faucets {
-		if f.CreatedAt.Year() == now.Year() &&
-			f.CreatedAt.Month() == now.Month() &&
-			f.CreatedAt.Day() == now.Day() {
-			return false
-		}
-	}
-
-	return true
-}
-
-//! Zealy Database
-
-func (db *DB) GetZealyUser(id string) (*zealy.ZealyUser, error) {
-	var u *zealy.ZealyUser
-	tx := db.Model(&zealy.ZealyUser{}).First(&u, "discord_id = ?", id)
-	if tx.Error != nil {
-		return &zealy.ZealyUser{}, ReadError{
-			Message: tx.Error.Error(),
-		}
-	}
-
-	return u, nil
-}
-
-func (db *DB) AddZealyUser(u *zealy.ZealyUser) error {
-	tx := db.Create(u)
-	if tx.Error != nil {
-		return WriteError{
-			Message: tx.Error.Error(),
-		}
-	}
-
-	return nil
-}
-
-func (db *DB) UpdateZealyUser(id string, txHash string) error {
-	tx := db.Model(&zealy.ZealyUser{
-		DiscordID: id,
-	}).Where("discord_id = ?", id).Update("tx_hash", txHash)
-	if tx.Error != nil {
-		return WriteError{
-			Message: tx.Error.Error(),
-		}
-	}
-
-	return nil
-}
-
-func (db *DB) GetAllZealyUser() ([]*zealy.ZealyUser, error) {
-	var u []*zealy.ZealyUser
-	tx := db.Find(&u)
-	if tx.Error != nil {
-		return nil, ReadError{
-			Message: tx.Error.Error(),
-		}
-	}
-
-	return u, nil
 }
---
+++ internal/repository/zealy.go
@@ -0,0 +1,51 @@
+package repository
+
+import "github.com/pagu-project/Pagu/internal/entity"
+
+func (db *DB) GetZealyUser(id string) (*entity.ZealyUser, error) {
+	var u *entity.ZealyUser
+	tx := db.Model(&entity.ZealyUser{}).First(&u, "discord_id = ?", id)
+	if tx.Error != nil {
+		return &entity.ZealyUser{}, ReadError{
+			Message: tx.Error.Error(),
+		}
+	}
+
+	return u, nil
+}
+
+func (db *DB) AddZealyUser(u *entity.ZealyUser) error {
+	tx := db.Create(u)
+	if tx.Error != nil {
+		return WriteError{
+			Message: tx.Error.Error(),
+		}
+	}
+
+	return nil
+}
+
+func (db *DB) UpdateZealyUser(id string, txHash string) error {
+	tx := db.Model(&entity.ZealyUser{
+		DiscordID: id,
+	}).Where("discord_id = ?", id).Update("tx_hash", txHash)
+	if tx.Error != nil {
+		return WriteError{
+			Message: tx.Error.Error(),
+		}
+	}
+
+	return nil
+}
+
+func (db *DB) GetAllZealyUser() ([]*entity.ZealyUser, error) {
+	var u []*entity.ZealyUser
+	tx := db.Find(&u)
+	if tx.Error != nil {
+		return nil, ReadError{
+			Message: tx.Error.Error(),
+		}
+	}
+
+	return u, nil
+}
