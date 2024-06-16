2697f7b65962925bf178621fdb90f8d0be6053e8
mj <akbarian.dev@gmail.ccom>
2024-06-17 01:06:21 +0330

    fix(phoenix faucet): call faucetHandler command

--- internal/engine/command/phoenix/phoenix.go
+++ internal/engine/command/phoenix/phoenix.go
@@ -1,6 +1,8 @@
 package phoenix
 
 import (
+	"fmt"
+
 	"github.com/pagu-project/Pagu/internal/engine/command"
 	"github.com/pagu-project/Pagu/internal/entity"
 	"github.com/pagu-project/Pagu/internal/repository"
@@ -39,23 +41,16 @@ func (pt *Phoenix) GetCommand() command.Command {
 	middlewareHandler := command.NewMiddlewareHandler(&pt.db, pt.wallet)
 
 	/*
-		subCmdFaucet := command.Command{
-			Name: FaucetCommandName,
-			Desc: fmt.Sprintf("Get %d tPAC Coins on Phoenix Testnet for Testing your code or project", pt.faucetAmount),
-			Help: "There is a limit that you can only get faucets 1 time per day with each user ID and address",
-			Args: []command.Args{
-				{
-					Name:     "address",
-					Desc:     "your testnet address [example: tpc1z...]",
-					Optional: false,
-				},
-			},
-			SubCommands: nil,
-			AppIDs:      entity.AllAppIDs(),
-			Middlewares: []command.MiddlewareFunc{middlewareHandler.CreateUser, middlewareHandler.WalletBalance},
-			MiddlewareHandler:     pt.faucetHandler,
-		}
 
+		subCmdWallet := command.Command{
+				Name:        WalletCommandName,
+				Desc:        "Check the status of Pagu faucet address wallet on Phoenix network",
+				Help:        "",
+				Args:        nil,
+				SubCommands: nil,
+				AppIDs:      entity.AllAppIDs(),
+				Handler:     pt.walletHandler,
+			}
 
 		subCmdHealth := command.Command{
 			Name:        HealthCommandName,
@@ -95,14 +90,21 @@ func (pt *Phoenix) GetCommand() command.Command {
 		Handler:     pt.networkStatusHandler,
 	}
 
-	subCmdWallet := command.Command{
-		Name:        WalletCommandName,
-		Desc:        "Check the status of Pagu faucet address wallet on Phoenix network",
-		Help:        "",
-		Args:        nil,
+	subCmdFaucet := command.Command{
+		Name: FaucetCommandName,
+		Desc: fmt.Sprintf("Get %d tPAC Coins on Phoenix Testnet for Testing your code or project", pt.faucetAmount),
+		Help: "There is a limit that you can only get faucets 1 time per day with each user ID and address",
+		Args: []command.Args{
+			{
+				Name:     "address",
+				Desc:     "your testnet address [example: tpc1z...]",
+				Optional: false,
+			},
+		},
 		SubCommands: nil,
 		AppIDs:      entity.AllAppIDs(),
-		Handler:     pt.walletHandler,
+		Middlewares: []command.MiddlewareFunc{middlewareHandler.CreateUser, middlewareHandler.WalletBalance},
+		Handler:     pt.faucetHandler,
 	}
 
 	cmdPhoenix := command.Command{
@@ -115,10 +117,10 @@ func (pt *Phoenix) GetCommand() command.Command {
 		Handler:     nil,
 	}
 
-	// cmdPhoenix.AddSubCommand(subCmdFaucet)
 	// cmdPhoenix.AddSubCommand(subCmdHealth)
 	// cmdPhoenix.AddSubCommand(subCmdNodeInfo)
-	cmdPhoenix.AddSubCommand(subCmdWallet)
+	// cmdPhoenix.AddSubCommand(subCmdWallet)
+	cmdPhoenix.AddSubCommand(subCmdFaucet)
 	cmdPhoenix.AddSubCommand(subCmdStatus)
 
 	return cmdPhoenix
--- internal/engine/command/phoenix/wallet.go
+++ internal/engine/command/phoenix/wallet.go
@@ -5,6 +5,7 @@ import (
 	"github.com/pagu-project/Pagu/internal/entity"
 )
 
+// nolint
 func (pt *Phoenix) walletHandler(cmd command.Command, _ entity.AppID, _ string, _ ...string) command.CommandResult {
 	return cmd.SuccessfulResult("Pagu Phoenix Address: %s\nBalance: %d", pt.wallet.Address(), pt.wallet.Balance())
 }
